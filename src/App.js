54b20eacf7bee1dc79502aea9f2496c9b85aab10
SSOL7 <soltani.shaho@gmail.com>
2023-04-25 11:14:23 +0300

    game in progress

--- src/App.js
+++ src/App.js
@@ -17,10 +17,88 @@ export default class App extends Component {
     }
   }
 
+  easylevel = () => {
+    this.setState({
+      circles: [
+        { id: 1, },
+        { id: 2, },
+        { id: 3, },
+      ],
+      score: 0,
+      time: 0,
+      game: true,
+    })
+    // shuffle background color of circles randomly on set interval
+    setInterval(() => {
+      if (this.state.game === true) {
+        const random = Math.floor(Math.random() * this.state.circles.length)
+        const circles = this.state.circles
+        circles[random].color = 'red'
+        this.setState({
+          circles: circles,
+        })
+        setTimeout(() => {
+          circles[random].color = 'white'
+          this.setState({
+            circles: circles,
+          })
+        }, 2000)
+      }
+    }
+      , 1000)
+      
+  }
+
+  mediumlevel = () => {
+    this.setState({
+      circles: [
+        { id: 1, },
+        { id: 2, },
+        { id: 3, },
+        { id: 4, },
+      ],
+      score: 0,
+      time: 0,
+      game: true,
+    })
+    // shuffle background color of circles randomly on set interval
+    setInterval(() => {
+      if (this.state.game === true) {
+        const random = Math.floor(Math.random() * this.state.circles.length)
+        const circles = this.state.circles
+        circles[random].color = 'red'
+        this.setState({
+          circles: circles,
+        })
+        setTimeout(() => {
+          circles[random].color = 'white'
+          this.setState({
+            circles: circles,
+          })
+        }, 2000)
+      }
+    }
+      , 1000)
+  }
+
+  hardlevel = (active, index) => {
+    this.setState({
+      circles: [
+        { id: 1, },
+        { id: 2, },
+        { id: 3, },
+        { id: 4, },
+        { id: 5, },
+      ],
+      score: 0,
+      time: 0,
+      game: true,
+    })
+  }
+
   render() {
     return (
       <div className='App-header'>
-
         {this.state.circles.map((circle) => (
           <div onClick={this.speedgame}
             key={circle.id}
@@ -34,11 +112,18 @@ export default class App extends Component {
             </div>
         ))}
         <div>
+      </div>
+
+        <div>
+          <button className='easy' onClick={this.easylevel} >Easy level</button>
+          <button className='medium' onClick={this.mediumlevel} >Medium level</button>
+          <button className='hard' onClick={this.hardlevel} >Hard level</button>
         </div>
 
-        <button onClick={this.easylevel} >Easy level</button>
-        <button onClick={this.mediumlevel} >Medium level</button>
-        <button onClick={this.hardlevel} >Hard level</button>
+        <div>
+          <h2>Score: {this.state.score}</h2>
+          <h2>Time: {this.state.time}</h2>
+        </div>
 
       </div>
     )
