78be5db51d2792d5922c44d23dad9aa5e0beb41a
SSOL7 <soltani.shaho@gmail.com>
2023-04-26 11:10:26 +0300

    add functionality to buttons with few bugs and background color not working WIP

--- src/App.js
+++ src/App.js
@@ -102,7 +102,7 @@ export default class App extends Component {
       time: 0,
       game: true,
     })
-    // shuffle blue color background of circles randomly on set interval and if other color than blue is clicked on, alert the user that they lost
+    // shuffle green color background of circles randomly on set interval
     setInterval(() => {
       if (this.state.game === true) {
         const random = Math.floor(Math.random() * this.state.circles.length)
@@ -118,13 +118,100 @@ export default class App extends Component {
           })
         }, 1000)
       }
-    } ,1000)
+    }
+    ,1000)
+    // each time a blue circle is clicked on, add 1 to score
+    if (this.state.circles.backgroundColor === 'blue') {
+      this.setState({
+        score: this.state.score + 1,
+      })
+    }
+    // each time a non blue circle is clicked on, subtract 1 from score
+    else {
+      this.setState({
+        score: this.state.score - 1,
+      })
+    }
   }
-    
+
+  startGame = (id, active) => {
+    this.setState({
+      game: true,
+    })
+    // shuffle the id of the circles randomly on set interval and if active id is clicked on, add 1 to score
+    setInterval(() => {
+      if (this.state.game === true) {
+        const random = Math.floor(Math.random() * this.state.circles.length)
+        const circles = this.state.circles
+        circles[random].color = 'blue'
+        this.setState({
+          circles: circles,
+        })
+        setTimeout(() => {
+          circles[random].color = 'white'
+          this.setState({
+            circles: circles,
+          })
+        }, 3000)
+      }
+    }
+    ,1000)
+    if (this.state.circles.backgroundColor === 'blue') {
+      this.setState({
+        score: this.state.score + 1,
+      })
+    } else {
+      this.setState({
+        score: this.state.score - 1,
+      })
+    }
+  }
+
+  speedgame = (id, active) => {
+    this.setState({
+      game: true,
+    })
+    // shuffle the id of the circles randomly on set interval and if active id is clicked on, add 1 to score
+    setInterval(() => {
+      if (this.state.game === true) {
+        const random = Math.floor(Math.random() * this.state.circles.length)
+        const circles = this.state.circles
+        circles[random].color = 'blue'
+        this.setState({
+          circles: circles,
+        })
+        setTimeout(() => {
+          circles[random].color = 'white'
+          this.setState({
+            circles: circles,
+          })
+        }, 1000)
+      }
+    }
+    ,1000)
+    if (this.state.circles.backgroundColor === 'blue') {
+      this.setState({
+        score: this.state.score + 1,
+      })
+    } else {
+      this.setState({
+        score: this.state.score - 1,
+      })
+    }
+  }
+  
 
   render() {
     return (
       <div className='App-header'>
+
+        <div onClick={this.clickcircle} className='circle1'></div>
+        <div onClick={this.clickcircle} className='circle2'></div>
+        <div onClick={this.clickcircle} className='circle3'></div>
+
+        <button className='start' onClick={this.startGame}>Start Game</button>
+        <button className='end' onClick={this.stopGame}>Stop Game</button>
+
         {this.state.circles.map((circle) => (
           <div onClick={this.speedgame}
             key={circle.id}
@@ -145,6 +232,8 @@ export default class App extends Component {
           <button className='medium' onClick={this.mediumlevel} >Medium level</button>
           <button className='hard' onClick={this.hardlevel} >Hard level</button>
         </div>
+
+
 
         <div>
           <h2>Score: {this.state.score}</h2>
